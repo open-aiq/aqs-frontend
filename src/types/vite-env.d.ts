916de0249ab198c8fa6d7660d918d37bacf962b9
huzaifah0x00 <huzaifah.asif.b@gmail.com>
2025-11-08 16:28:25 +0500

    Delete everything and start over with react+vite+shadcn+clerk

---
+++ src/types/vite-env.d.ts
@@ -0,0 +1,11 @@
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_CLERK_PUBLISHABLE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
